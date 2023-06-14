21743171daf2094379b91dfb74bf7c15049b729f
Karl Pertsch <karl.pertsch@gmail.com>
2023-06-14 16:15:09 -0700

    Remove dm_control and mjrl dependencies

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
                       'mujoco_py', 
                       'h5py', 
                       'termcolor', # adept_envs dependency
-                      'click',  # adept_envs dependency
-                      'dm_control',
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                      'click'],
+                      #'dm_control',
+                      #'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
 )
